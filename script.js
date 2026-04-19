6704493b8dcd3f529a4820d4f10ec6556c00145f
Kiara Boabilwe <pinkkiara55@gmail.com>
2026-04-19 22:23:49 +0200

    Update

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ setInterval(() => {
     index = (index + 1) % slides.length;
     slides[index].classList.add("active");
 }, 4000);
+
+const searchBar = document.getElementById("searchBar");
+const products = document.querySelectorAll(".product");
+
+searchBar.addEventListener("keyup", function () {
+  const value = this.value.toLowerCase();
+
+  products.forEach(product => {
+    const name = product.getAttribute("data-name").toLowerCase();
+
+    if (name.includes(value)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+});
+
+document.getElementById("cartBtn").addEventListener("click", function () {
+  alert("Cart opened!");
+});
+
+const images = [
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg"
+];
+
+let index = 0;
+const hero = document.querySelector(".hero");
+
+function changeBackground() {
+  hero.style.backgroundImage = `url(${images[index]})`;
+  index = (index + 1) % images.length;
+}
+
+changeBackground();
+setInterval(changeBackground, 5000);
