d2091f27be07899e78c6b5bc5dff799cdc3c4b40
Kiara Boabilwe <pinkkiara55@gmail.com>
2026-04-21 14:59:18 +0200

    Update

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function addToCart() {
     let count = document.getElementById("cart-count");
     count.textContent = parseInt(count.textContent) + 1;
 }
+
+// HERO IMAGE SLIDER
+let slides = document.querySelectorAll(".hero-slide");
+let current = 0;
+
+setInterval(() => {
+    slides[current].classList.remove("active");
+
+    current = (current + 1) % slides.length;
+
+    slides[current].classList.add("active");
+}, 4000);
+
+
+// BUTTON NAVIGATION
+function goToShop() {
+    window.location.href = "clothing.html"; // change if needed
+}
+
+function goToSales() {
+    window.location.href = "promotions.html";
+}
